c5709e18f84bee5f72f80d53178acc45a9447675
noelnwaghodoh <monstershq@outlook.com>
2026-03-24 13:05:10 +0000

    first commit

---
+++ database.js
@@ -0,0 +1,80 @@
+import mysql from "mysql2";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+export const pool = mysql
+  .createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  })
+  .promise();
+
+export async function getUsers() {
+  const [rows] = await pool.query("SELECT * FROM Users;");
+  return rows;
+}
+
+export async function getUser(id) {
+  const [rows] = await pool.query(
+    `
+    SELECT * 
+    FROM Users
+    WHERE id = ?;
+    `,
+    [id],
+  );
+  return rows[0];
+}
+
+export async function getBook(id) {
+  const [rows] = await pool.query(
+    `
+    SELECT * 
+    FROM Catalogue
+    WHERE BookID = ?;
+    `,
+    [id],
+  );
+  return rows[0];
+}
+
+export async function addBook(values) {
+  console.log(values);
+  const [result] = await pool.query(
+    `
+      INSERT into Catalogue (BookTitle,BookSubjects,BookPublisher,BookIdentifier,BookEdition,BookDate,BookContributors,BookAuthor,BookFileName)
+      Values(?,?,?,?,?,?,?,?,?)
+    `,
+    [
+      values.bookTitle,
+      values.bookSubjects,
+      values.bookPublisher,
+      values.bookIdentifier,
+      values.bookEdition,
+      values.bookReleaseDate,
+      values.bookContributors,
+      values.bookAuthor,
+      values.bookFileName,
+    ],
+  );
+  const id = [result.insertId];
+  return getBook(id);
+}
+
+export async function login(email, password) {
+  console.log(email + " " + password);
+  const [rows] = await pool.query(
+    `
+    SELECT * 
+    FROM Users
+    WHERE UserEmail = ? and UserPassword =?;
+    `,
+    [email, password],
+  );
+  return rows;
+}
+
+const result = await getUsers();
---
+++ index.js
@@ -0,0 +1,141 @@
+import express, { request } from "express";
+import data from "./data/mock.json" with { type: "json" };
+import cors from "cors";
+import { getUsers, getUser, login } from "./database.js";
+import { pool } from "./database.js";
+import req from "express/lib/request.js";
+import { S3 } from "@aws-sdk/client-s3";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { addBook } from "./database.js";
+//import res from "express/lib/response";
+
+const app = express();
+
+/*const s3Client = new S3({
+    forcePathStyle: false, // Configures to use subdomain/virtual calling format.
+    endpoint: "https://cataloguefiles.lon1.digitaloceanspaces.com",
+    region: "us-east-1",
+    credentials: {
+      accessKeyId: process.env.SPACES_KEY,
+      secretAccessKey: process.env.SPACES_SECRET
+    }
+});*/
+
+/*const signS3URL = async (req, res, next) => {
+    const { fileName, fileType } = req.query;
+    const s3Params = {
+        Bucket: process.env.S3_BUCKET,
+        Key: fileName,
+        ContentType: fileType,
+        // ACL: 'bucket-owner-full-control'
+    };
+    const s3 = new S3({
+    forcePathStyle: false, // Configures to use subdomain/virtual calling format.
+    endpoint: "https://cataloguefiles.lon1.digitaloceanspaces.com",
+    region: "us-east-1",
+    credentials: {
+      accessKeyId: process.env.SPACES_KEY,
+      secretAccessKey: process.env.SPACES_SECRET
+    }
+});
+    const command = new PutObjectCommand(s3Params);
+
+    try {
+        const signedUrl = await getSignedUrl(s3, command, { expiresIn: 60 });
+        console.log(signedUrl);
+        res.json({ signedUrl })
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+}*/
+
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.json());
+
+let x = null;
+
+app.get("/users", async (request, response) => {
+  const users = await getUsers();
+  response.send(users);
+  console.log(users);
+  // res.json();
+});
+
+app.post("/catalogue", async (req, res) => {
+  const newBook = addBook(req.body);
+  res.send(newBook);
+});
+
+app.get("/upload", async (req, res) => {
+  console.log("pong");
+  console.log("this is " + req.query.fileName);
+  const { fileName, fileType } = req.query;
+  const s3Params = {
+    Bucket: process.env.S3_BUCKET,
+    Key: fileName,
+    ContentType: fileType,
+    // ACL: 'bucket-owner-full-control'
+  };
+  const s3 = new S3Client({
+    forcePathStyle: false, // Configures to use subdomain/virtual calling format.
+    endpoint: "https://cataloguefiles.lon1.digitaloceanspaces.com",
+    region: "us-east-1",
+    credentials: {
+      accessKeyId: process.env.SPACES_KEY,
+      secretAccessKey: process.env.SPACES_SECRET,
+    },
+  });
+  const command = new PutObjectCommand(s3Params);
+
+  try {
+    const signedUrl = await getSignedUrl(s3, command, { expiresIn: 3600 });
+    console.log(signedUrl);
+    res.json({ signedUrl });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.get("/users/:id", async (request, response) => {
+  const id = request.params.id;
+  const note = await getNote(id);
+  response.send(note);
+  // console.log(result);
+  // res.json();
+});
+
+app.post("/login", async (request, response) => {
+  console.log("the request works");
+  const sql = "SELECT *FROM users WHERE UserEmail = ? and UserPassword =?";
+  /* pool.query(sql, ["k1234567@kingston.ac.uk", "1234567"], (err, result) => {
+    console.log("the query runs ");
+    if (err) return response.json({ Message: "Error inside server" });
+    if (result.length > 0) {
+      return response.json({ Login: true, usertype: result[0].UserTypeID });
+    } else {
+      return response.json({ Login: false });
+    }
+  });*/
+  const user = await login(request.body.email, request.body.password);
+  console.log(user);
+  if (user) {
+    response.json({ Login: true, usertype: user[0].UserTypeID });
+  } else {
+    response.json({ Login: false });
+  }
+});
+
+app.post("/edit", (request, response) => {
+  response.send("This is PUT request at /edit");
+});
+app.post("/delete", (request, response) => {
+  response.send("This is DELETE request at /delte");
+});
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log(`The server is runnig on port ${PORT}`);
+  //console.log(data);
+});
